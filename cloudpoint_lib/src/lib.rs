2cd78d6fd55a2f1f6c54a40008bc338a1ebf5715
Dan Walker <git@dwalker.me>
2026-03-18 14:36:35 +0000

    Replace manual netcode with minreq, which works fine on 3ds (ureq didn't)

--- cloudpoint_lib/src/lib.rs
+++ cloudpoint_lib/src/lib.rs
@@ -1,4 +1,3 @@
 pub mod title;
 pub mod version;
-pub mod net;
 
--- cloudpoint_lib/src/version.rs
+++ cloudpoint_lib/src/version.rs
@@ -1,7 +1,36 @@
-use crate::net;
-use anyhow::{anyhow, Context};
+use anyhow::{anyhow, Context, Result};
 use chrono::{DateTime, Utc};
-use std::net::ToSocketAddrs;
+use itertools::Itertools;
+
+#[derive(Debug, serde::Deserialize)]
+pub struct VersionDirList(Vec<VersionDirEntry>);
+
+impl VersionDirList {
+    pub fn try_get(base_url: &str, user_key: &str, title_id: u64) -> Result<VersionDirList> {
+        let url = format!("{base_url}/sync/{user_key}/titles/{title_id}/save/");
+
+        let res = minreq::get(url)
+            .with_header("Accept", "application/json")
+            .send()?;
+
+        match res.status_code {
+            200 => Ok(serde_json::from_slice(&res.as_bytes())?),
+            404 => Ok(VersionDirList(Vec::with_capacity(0))),
+            _ => Err(anyhow!(
+                "version dir lookup failed, HTTP {}",
+                res.status_code
+            )),
+        }
+    }
+
+    pub fn latest(&self) -> Option<&VersionDirEntry> {
+        self.0
+            .as_slice()
+            .iter()
+            .sorted_by(|a, b| a.mod_time.cmp(&b.mod_time))
+            .last()
+    }
+}
 
 #[derive(Debug, serde::Deserialize)]
 pub struct VersionDirEntry {
@@ -11,16 +40,11 @@ pub struct VersionDirEntry {
 }
 
 impl VersionDirEntry {
-    pub fn fingerprint(&self) -> anyhow::Result<u64> {
-        self.name.parse().context("{self.name} is not named with a valid fingerprint")
+    pub fn fingerprint(&self) -> Result<u64> {
+        self.name
+            .parse()
+            .context("{self.name} is not named with a valid fingerprint")
     }
-}
-
-pub fn get_version_dir_entries(
-    host: impl ToSocketAddrs,
-    path: &str,
-) -> anyhow::Result<Vec<VersionDirEntry>> {
-    Ok(serde_json::from_slice(&net::get_body(host, path)?)?)
 }
 
 #[cfg(test)]
@@ -28,9 +52,12 @@ mod tests {
     use super::*;
     use httpmock::prelude::*;
 
+    const USER_KEY: &str = "test_user_key";
+    const TITLE_ID: u64 = 0x000400001234ABCD;
+
     #[test]
     fn fingerprint_fails_on_malformed_name() {
-        let e = VersionDirEntry{
+        let e = VersionDirEntry {
             name: "foobar".into(),
             size: 123,
             mod_time: Default::default(),
@@ -41,7 +68,7 @@ mod tests {
 
     #[test]
     fn fingerprint_ok_on_valid_name() {
-        let e = VersionDirEntry{
+        let e = VersionDirEntry {
             name: "987654321".into(),
             size: 123,
             mod_time: Default::default(),
@@ -52,11 +79,9 @@ mod tests {
 
     #[test]
     fn can_get_dir_listing() {
-        let path = "/sync/abc123/titles/000400001234ABCD/save/";
-
         let srv = MockServer::start();
         srv.mock(|when, then| {
-            when.method("GET").path(path);
+            when.method("GET").path(format!("/sync/{USER_KEY}/titles/{TITLE_ID}/save/"));
             then.status(200).body(
                 r#"[
                     {"name":"12345678","size":0,"mod_time":"2026-03-16T14:26:22.425706984Z"},
@@ -65,7 +90,22 @@ mod tests {
             );
         });
 
-        let res = get_version_dir_entries((srv.host(), srv.port()), path);
+        let res = VersionDirList::try_get(&srv.base_url(), USER_KEY, TITLE_ID);
+
         assert!(res.is_ok());
+        assert_eq!(res.unwrap().0.len(), 2);
+    }
+
+    #[test]
+    fn can_get_empty_dir_listing_for_404() {
+        let srv = MockServer::start();
+        srv.mock(|_, then| {
+            then.status(404);
+        });
+
+        let res = VersionDirList::try_get(&srv.base_url(), USER_KEY, TITLE_ID);
+
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap().0.len(), 0);
     }
 }
