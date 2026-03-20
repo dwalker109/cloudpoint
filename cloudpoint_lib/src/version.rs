54eb8b2339f0015ebe3164f0237fb05ef1908d80
Dan Walker <git@dwalker.me>
2026-03-20 11:41:54 +0000

    Lib version support for fetching and pushing to remote

--- cloudpoint_lib/src/version.rs
+++ cloudpoint_lib/src/version.rs
@@ -1,5 +1,6 @@
-use anyhow::{anyhow, Context, Result};
+use anyhow::{Context, Result, anyhow};
 use chrono::{DateTime, Utc};
+use chunktree::{tree::Leaf, version::Version};
 use itertools::Itertools;
 
 #[derive(Debug, serde::Deserialize)]
@@ -17,8 +18,9 @@ impl VersionDirList {
             200 => Ok(serde_json::from_slice(&res.as_bytes())?),
             404 => Ok(VersionDirList(Vec::with_capacity(0))),
             _ => Err(anyhow!(
-                "version dir lookup failed, HTTP {}",
-                res.status_code
+                "version dir lookup failed, HTTP {}, {}",
+                res.status_code,
+                res.reason_phrase
             )),
         }
     }
@@ -45,12 +47,64 @@ impl VersionDirEntry {
             .parse()
             .context("{self.name} is not named with a valid fingerprint")
     }
+
+    pub fn get_version<T: Leaf>(
+        &self,
+        base_url: &str,
+        user_key: &str,
+        title_id: u64,
+    ) -> Result<Version<T>> {
+        let url = format!(
+            "{base_url}/sync/{user_key}/titles/{title_id}/save/{}",
+            self.name
+        );
+
+        let res = minreq::get(url).send()?;
+
+        match res.status_code {
+            200 => Ok(postcard::from_bytes(res.as_bytes())?),
+            _ => Err(anyhow!(
+                "version file download failed, HTTP {}. {}",
+                res.status_code,
+                res.reason_phrase
+            )),
+        }
+    }
+
+    pub fn put_version<T: Leaf>(
+        base_url: &str,
+        user_key: &str,
+        title_id: u64,
+        version: &Version<T>,
+    ) -> Result<()> {
+        let url = format!(
+            "{base_url}/sync/{user_key}/titles/{title_id}/save/{}",
+            version.fingerprint(),
+        );
+
+        let res = minreq::put(url)
+            .with_body(postcard::to_allocvec(&version)?)
+            .send()?;
+
+        match res.status_code {
+            201 => Ok(()),
+            _ => Err(anyhow!(
+                "version file upload failed, HTTP {}. {}",
+                res.status_code,
+                res.reason_phrase
+            )),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::{BTreeSet, HashMap};
+
     use super::*;
+    use chunktree::tree::{MemLeaf, Tree};
     use httpmock::prelude::*;
+    use serde::Serialize;
 
     const USER_KEY: &str = "test_user_key";
     const TITLE_ID: u64 = 0x000400001234ABCD;
@@ -81,11 +135,12 @@ mod tests {
     fn can_get_dir_listing() {
         let srv = MockServer::start();
         srv.mock(|when, then| {
-            when.method("GET").path(format!("/sync/{USER_KEY}/titles/{TITLE_ID}/save/"));
+            when.method("GET")
+                .path(format!("/sync/{USER_KEY}/titles/{TITLE_ID}/save/"));
             then.status(200).body(
                 r#"[
-                    {"name":"12345678","size":0,"mod_time":"2026-03-16T14:26:22.425706984Z"},
-                    {"name":"abcde123","size":0,"mod_time":"2026-03-17T12:04:29.799632917Z"}
+                    {"name":"12345678","size":123,"mod_time":"2026-03-16T14:26:22.425706984Z"},
+                    {"name":"abcde123","size":456,"mod_time":"2026-03-17T12:04:29.799632917Z"}
                 ]"#,
             );
         });
@@ -107,5 +162,122 @@ mod tests {
 
         assert!(res.is_ok());
         assert_eq!(res.unwrap().0.len(), 0);
+    }
+
+    #[test]
+    fn can_get_version() {
+        //TODO! Get a fixture for this instead of duck typing something
+        #[derive(Serialize)]
+        struct DuckVersion {
+            payload: BTreeSet<()>,
+            meta: HashMap<String, ()>,
+        }
+
+        let srv = MockServer::start();
+        srv.mock(|when, then| {
+            when.method("GET")
+                .path(format!("/sync/{USER_KEY}/titles/{TITLE_ID}/save/12345678"));
+            then.status(200).body(
+                postcard::to_allocvec(&DuckVersion {
+                    payload: BTreeSet::default(),
+                    meta: HashMap::default(),
+                })
+                .unwrap(),
+            );
+        });
+
+        let e = VersionDirEntry {
+            name: "12345678".into(),
+            size: 123,
+            mod_time: chrono::DateTime::default(),
+        };
+        let res = e.get_version::<MemLeaf>(&srv.base_url(), USER_KEY, TITLE_ID);
+
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn version_get_fails_on_malformed_data() {
+        let srv = MockServer::start();
+        srv.mock(|when, then| {
+            when.method("GET")
+                .path(format!("/sync/{USER_KEY}/titles/{TITLE_ID}/save/12345678"));
+            then.status(200)
+                .body(postcard::to_allocvec(b"junk bytes").unwrap());
+        });
+
+        let e = VersionDirEntry {
+            name: "12345678".into(),
+            size: 123,
+            mod_time: chrono::DateTime::default(),
+        };
+        let res = e.get_version::<MemLeaf>(&srv.base_url(), USER_KEY, TITLE_ID);
+
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn version_get_fails_on_missing_data() {
+        let srv = MockServer::start();
+        srv.mock(|when, then| {
+            when.method("GET");
+            then.status(404);
+        });
+
+        let e = VersionDirEntry {
+            name: "12345678".into(),
+            size: 123,
+            mod_time: chrono::DateTime::default(),
+        };
+        let res = e.get_version::<MemLeaf>(&srv.base_url(), USER_KEY, TITLE_ID);
+
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn version_put_succeeds_on_new_file() {
+        let v = Version::<MemLeaf>::new(
+            &Tree::new(Vec::default(), ()),
+            HashMap::default(),
+            128,
+            512,
+            1024,
+        )
+        .unwrap();
+
+        let srv = MockServer::start();
+        srv.mock(|when, then| {
+            when.method("PUT").path(format!(
+                "/sync/{USER_KEY}/titles/{TITLE_ID}/save/{}",
+                v.fingerprint()
+            ));
+            then.status(201);
+        });
+
+        let res = VersionDirEntry::put_version(&srv.base_url(), USER_KEY, TITLE_ID, &v);
+
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn version_put_fails_on_existing_file() {
+        let v = Version::<MemLeaf>::new(
+            &Tree::new(Vec::default(), ()),
+            HashMap::default(),
+            128,
+            512,
+            1024,
+        )
+        .unwrap();
+
+        let srv = MockServer::start();
+        srv.mock(|when, then| {
+            when.method("PUT");
+            then.status(409);
+        });
+
+        let res = VersionDirEntry::put_version(&srv.base_url(), USER_KEY, TITLE_ID, &v);
+
+        assert!(res.is_err());
     }
 }
