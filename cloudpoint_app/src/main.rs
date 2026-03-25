6d471e44ef953065c53d9058d62bfcbe0f26029d
Dan Walker <git@dwalker.me>
2026-03-25 11:11:25 +0000

    Title sync framework
    * Title sync type setup and basics
    * Use minreq, which works fine on 3ds (ureq didn't)
    * Lib version support for fetching and pushing to remote
    * Trait impl for Ctr archive FS
    * Switch from broken Caddy to horribly complex but working Lua driven nginx
    * Support basic sync conflict resolution and clearing secure save value on
    download, plus better fsInit/fsExit usage
    * Working sync of saves and extdata including subdir creation!
    
    Messy, needs a big cleanup.

--- cloudpoint_app/src/main.rs
+++ cloudpoint_app/src/main.rs
@@ -1,3 +1,295 @@
-fn main() {
-    println!("Hello, world!");
+mod archive;
+mod store;
+mod config {
+    pub const BASE_URL: &'static str = "http://192.168.1.45:8080";
+    pub const USER_KEY: &'static str = "dw";
+}
+
+use anyhow::{Context, Result};
+use chunktree::{
+    store::MemStore,
+    tree::Tree,
+    version::{Diff, Version, updater::BlockingUpdater},
+};
+use cloudpoint_lib::{
+    sync::{CtrArchiveMode, SyncAction, SyncState},
+    version::VersionDirEntry,
+};
+use ctru::{
+    console::Console,
+    services::{
+        am::{Am, Title},
+        apt::Apt,
+        fs::MediaType,
+        gfx::Gfx,
+        hid::{Hid, KeyPad},
+        soc::Soc,
+    },
+};
+use std::{
+    collections::HashMap,
+    fs::{self, File, create_dir_all, read_to_string},
+};
+
+use crate::archive::{CtrArchiveLeaf, walk_tree};
+use crate::config::{BASE_URL, USER_KEY};
+use crate::store::HttpStore;
+
+fn main() -> Result<()> {
+    let am = Am::new()?;
+    let apt = Apt::new()?;
+    let mut hid = Hid::new()?;
+    let gfx = Gfx::new()?;
+    let _console = Console::new(gfx.top_screen.borrow_mut());
+    let mut _soc = Soc::new()?;
+
+    // soc.redirect_to_3dslink(true, true)?;
+
+    let installed_titles = get_installed_titles(&am)?;
+
+    setup_sdmc()?;
+    autoadd(&installed_titles, CtrArchiveMode::Savedata)?;
+    autoadd(&installed_titles, CtrArchiveMode::Extdata)?;
+
+    let mut sync_states = get_sync_states()?;
+    let mut installed_sync_states = get_installed_sync_states(&sync_states, &installed_titles);
+
+    println!("Available sync states: {:?}", sync_states.len());
+    println!("Active sync states: {:?}", installed_sync_states.len());
+    println!("Press (A) to sync");
+    println!("Press Start to exit");
+
+    while apt.main_loop() {
+        gfx.wait_for_vblank();
+
+        hid.scan_input();
+
+        if hid.keys_down().contains(KeyPad::START) {
+            break;
+        }
+
+        if hid.keys_down().contains(KeyPad::A) {
+            let res = do_sync(&apt, &mut hid, &gfx, installed_sync_states.clone());
+            println!("Results: {:?}", res);
+
+            sync_states = get_sync_states()?;
+            installed_sync_states = get_installed_sync_states(&sync_states, &installed_titles);
+        }
+    }
+
+    Ok(())
+}
+
+fn setup_sdmc() -> Result<()> {
+    let paths = [
+        "sdmc:/3ds/Cloudpoint",
+        "sdmc:/3ds/Cloudpoint/db",
+        "sdmc:/3ds/Cloudpoint/autoadd",
+    ];
+    for p in paths {
+        create_dir_all(p).with_context(|| format!("fatal: failed to create directory {p}"))?;
+    }
+
+    Ok(())
+}
+
+fn autoadd(installed_titles: &[Title], archive_mode: CtrArchiveMode) -> Result<()> {
+    for add_code in
+        read_to_string(format!("sdmc:/3ds/Cloudpoint/autoadd/{archive_mode}.txt"))?.lines()
+    {
+        if let Some((title_id, product_code)) = installed_titles
+            .iter()
+            .map(|t| (t.id(), t.product_code().trim_end_matches('\0').to_string()))
+            .find(|t| t.1 == add_code)
+        {
+            let state = SyncState {
+                title_id,
+                product_code,
+                archive_mode,
+                last_fp: None,
+                local_fp: None,
+                remote_fp: None,
+            };
+
+            let path = format!(
+                "sdmc:/3ds/Cloudpoint/db/{}.{}",
+                state.product_code, state.archive_mode
+            );
+            if !fs::exists(&path)? {
+                fs::write(path, serde_json::to_string_pretty(&state)?)?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn get_sync_states() -> Result<Vec<SyncState>> {
+    let mut states: Vec<SyncState> = Vec::new();
+
+    for f in fs::read_dir("sdmc:/3ds/Cloudpoint/db")? {
+        let f = f?;
+        if let Ok(s) = serde_json::from_reader(File::open(f.path())?) {
+            states.push(s);
+        }
+    }
+
+    Ok(states)
+}
+
+fn get_installed_titles(am: &Am) -> Result<Vec<Title>> {
+    let titles = am.title_list(MediaType::Sd)?;
+
+    Ok(titles)
+}
+
+fn get_installed_sync_states(
+    sync_states: &[SyncState],
+    installed_titles: &[Title],
+) -> Vec<SyncState> {
+    let ids = installed_titles.iter().map(|t| t.id()).collect::<Vec<_>>();
+    sync_states
+        .iter()
+        .filter(|&s| ids.contains(&s.title_id))
+        .cloned()
+        .collect()
+}
+
+fn do_sync(apt: &Apt, hid: &mut Hid, gfx: &Gfx, active_sync_states: Vec<SyncState>) -> Result<()> {
+    for mut s in active_sync_states {
+        let list = cloudpoint_lib::version::VersionDirList::try_get(
+            BASE_URL,
+            "dw",
+            s.title_id,
+            s.archive_mode,
+        )?;
+        s.remote_fp = list.latest().and_then(|e| e.fingerprint().ok());
+
+        println!(
+            "Using remote version {:016x} for title {:016x}",
+            s.remote_fp.unwrap_or_default(),
+            s.title_id
+        );
+
+        let Ok(local_tree) = walk_tree(s.title_id, s.archive_mode) else {
+            //TODO! Check it actually has an archive? Or just assume on error we pull it down if it is there?
+            println!(
+                "Failed to get {:?} archive for title {:016x}, run once to init and retry",
+                s.archive_mode, s.title_id
+            );
+            continue;
+        };
+
+        let local_ver = Version::new(&local_tree, HashMap::default(), 128_000, 512_000, 1024_000)?;
+        s.local_fp = Some(local_ver.fingerprint());
+
+        println!(
+            "Using local version {:016x} for title {:016x}",
+            s.local_fp.unwrap_or_default(),
+            s.title_id
+        );
+
+        match s.get_action() {
+            SyncAction::Nothing => {
+                println!("Nothing to do for title {:016x}", s.title_id);
+            }
+            SyncAction::Conflict => {
+                println!("Title {:016x} changed on server and locally!", s.title_id);
+                println!("Press DPAD UP to upload (local wins)");
+                println!("Press DPAD DOWN to download (remote wins)");
+                println!("Press DPAD LEFT or DPAD RIGHT to skip (come back later)");
+
+                while apt.main_loop() {
+                    gfx.wait_for_vblank();
+                    hid.scan_input();
+
+                    if hid.keys_down().contains(KeyPad::DPAD_UP) {
+                        ul(&mut s, &local_ver, &local_tree)?;
+                        break;
+                    } else if hid.keys_down().contains(KeyPad::DPAD_DOWN) {
+                        dl(&mut s, &local_ver, local_tree)?;
+                        break;
+                    } else if hid
+                        .keys_down()
+                        .intersects(KeyPad::DPAD_LEFT | KeyPad::DPAD_RIGHT)
+                    {
+                        break;
+                    }
+                }
+            }
+            SyncAction::Upload => {
+                ul(&mut s, &local_ver, &local_tree)?;
+            }
+            SyncAction::Download => {
+                dl(&mut s, &local_ver, local_tree)?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn ul(
+    s: &mut SyncState,
+    local_ver: &Version<CtrArchiveLeaf>,
+    local_tree: &Tree<CtrArchiveLeaf>,
+) -> Result<()> {
+    let mut store = HttpStore(BASE_URL.into());
+    local_ver.copy_chunks(&local_tree, &mut store)?;
+    VersionDirEntry::put_version(BASE_URL, USER_KEY, s.title_id, s.archive_mode, &local_ver)?;
+
+    s.last_fp = Some(local_ver.fingerprint());
+    fs::write(
+        format!(
+            "sdmc:/3ds/Cloudpoint/db/{}.{}",
+            s.product_code, s.archive_mode
+        ),
+        serde_json::to_string_pretty(&s)?,
+    )
+    .context("writing local db state")?;
+
+    println!("Uploaded save for title {:016x}", s.title_id);
+
+    Ok(())
+}
+
+fn dl(
+    s: &mut SyncState,
+    local_ver: &Version<CtrArchiveLeaf>,
+    local_tree: Tree<CtrArchiveLeaf>,
+) -> Result<()> {
+    let Ok(remote_ver) = VersionDirEntry::get_version::<CtrArchiveLeaf>(
+        BASE_URL,
+        USER_KEY,
+        s.title_id,
+        s.archive_mode,
+        s.remote_fp
+            .expect("unreachable without a remote version available"),
+    ) else {
+        println!("failed to fetch version manifest for {:016x}", s.title_id);
+        return Ok(());
+    };
+
+    let diff = Diff::new(&local_ver, &remote_ver);
+    let cache = MemStore::default();
+    let store = HttpStore(BASE_URL.into());
+    let mut u = BlockingUpdater::start(diff, local_tree, cache, store)?;
+
+    while !u.is_terminal() {
+        u.update_next()?;
+    }
+
+    s.last_fp = Some(remote_ver.fingerprint());
+    fs::write(
+        format!(
+            "sdmc:/3ds/Cloudpoint/db/{}.{}",
+            s.product_code, s.archive_mode
+        ),
+        serde_json::to_string_pretty(&s)?,
+    )
+    .context("writing local db state")?;
+
+    println!("Downloaded save for title {:016x}", s.title_id);
+
+    Ok(())
 }
---
+++ cloudpoint_lib/src/sync.rs
@@ -0,0 +1,186 @@
+use std::fmt::Display;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SyncState {
+    pub title_id: u64,
+    pub product_code: String,
+    pub archive_mode: CtrArchiveMode,
+    pub last_fp: Option<u64>,
+    #[serde(skip)]
+    pub local_fp: Option<u64>,
+    #[serde(skip)]
+    pub remote_fp: Option<u64>,
+}
+
+impl SyncState {
+    pub fn get_action(&self) -> SyncAction {
+        match (self.local_fp, self.remote_fp) {
+            (None, Some(_)) => SyncAction::Download,
+            (Some(_), None) => SyncAction::Upload,
+            (None, None) => SyncAction::Nothing,
+            (Some(_), Some(_)) => {
+                let changed_local = self.local_fp != self.last_fp;
+                let changed_remote = self.remote_fp != self.last_fp;
+
+                match (changed_local, changed_remote) {
+                    (false, true) => SyncAction::Download,
+                    (true, false) => SyncAction::Upload,
+                    (false, false) => SyncAction::Nothing,
+                    (true, true) => SyncAction::Conflict,
+                }
+            }
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Serialize, Deserialize)]
+pub enum CtrArchiveMode {
+    Savedata,
+    Extdata,
+}
+
+impl Display for CtrArchiveMode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            CtrArchiveMode::Savedata => write!(f, "save"),
+            CtrArchiveMode::Extdata => write!(f, "extdata"),
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum SyncAction {
+    Nothing,
+    Conflict,
+    Upload,
+    Download,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn local_only_no_remote() {
+        let mut s = SyncState { ..fixture() };
+        s.local_fp = Some(1);
+
+        let res = s.get_action();
+
+        assert!(matches!(res, SyncAction::Upload));
+    }
+
+    #[test]
+    fn remote_only_no_local() {
+        let mut s = SyncState { ..fixture() };
+        s.remote_fp = Some(1);
+
+        let res = s.get_action();
+
+        assert!(matches!(res, SyncAction::Download));
+    }
+
+    #[test]
+    fn no_change() {
+        let s = fixture();
+
+        assert!(matches!(s.get_action(), SyncAction::Nothing))
+    }
+
+    #[test]
+    fn local_change_only() {
+        let s = SyncState {
+            last_fp: Some(1),
+            local_fp: Some(2),
+            remote_fp: Some(1),
+            ..fixture()
+        };
+
+        let res = s.get_action();
+
+        assert!(matches!(res, SyncAction::Upload));
+    }
+
+    #[test]
+    fn remote_change_only() {
+        let s = SyncState {
+            last_fp: Some(1),
+            local_fp: Some(1),
+            remote_fp: Some(2),
+            ..fixture()
+        };
+
+        let res = s.get_action();
+
+        assert!(matches!(res, SyncAction::Download));
+    }
+
+    #[test]
+    fn both_change() {
+        let s = SyncState {
+            last_fp: Some(1),
+            local_fp: Some(2),
+            remote_fp: Some(2),
+            ..fixture()
+        };
+
+        let res = s.get_action();
+
+        assert!(matches!(res, SyncAction::Conflict));
+    }
+
+    #[test]
+    fn no_local_with_remote_always_download() {
+        let s = SyncState {
+            last_fp: Some(1),
+            local_fp: None,
+            remote_fp: Some(1),
+            ..fixture()
+        };
+
+        let res = s.get_action();
+
+        assert!(matches!(res, SyncAction::Download));
+    }
+
+    #[test]
+    fn no_remote_with_local_always_upload() {
+        let s = SyncState {
+            last_fp: Some(1),
+            local_fp: Some(1),
+            remote_fp: None,
+            ..fixture()
+        };
+
+        let res = s.get_action();
+
+        assert!(matches!(res, SyncAction::Upload));
+    }
+
+    #[test]
+    fn no_remote_no_local_always_no_action() {
+        let s = SyncState {
+            last_fp: Some(1),
+            local_fp: None,
+            remote_fp: None,
+            ..fixture()
+        };
+
+        let res = s.get_action();
+
+        assert!(matches!(res, SyncAction::Nothing));
+    }
+
+    fn fixture() -> SyncState {
+        SyncState {
+            title_id: 0x00040000_1234ABCD,
+            product_code: "XTR-X-ABCD".into(),
+            archive_mode: CtrArchiveMode::Savedata,
+            last_fp: None,
+            local_fp: None,
+            remote_fp: None,
+        }
+    }
+}
