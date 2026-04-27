13660430df2d74329bf96aac3ee81ad5447b52f2
Dan Walker <git@dwalker.me>
2026-04-27 15:26:40 +0100

    Move userkey to a Uuid persisted into its own file on the console

--- cloudpoint_lib/src/settings.rs
+++ cloudpoint_lib/src/settings.rs
@@ -1,10 +1,10 @@
 use serde::{Deserialize, Serialize};
 use std::{fs, sync::LazyLock};
+use uuid::Uuid;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Settings {
     pub base_url: String,
-    pub user_key: String,
     pub log: String,
     pub backup: bool,
 }
@@ -12,8 +12,6 @@ pub struct Settings {
 pub static SETTINGS: LazyLock<Settings> = LazyLock::new(|| {
     config::Config::builder()
         .set_default("base_url", "http://192.168.1.45:8080")
-        .unwrap()
-        .set_default("user_key", "foobarbaz")
         .unwrap()
         .set_default("log", "off")
         .unwrap()
@@ -27,4 +25,22 @@ pub static SETTINGS: LazyLock<Settings> = LazyLock::new(|| {
         .unwrap_or_default()
         .try_deserialize::<Settings>()
         .unwrap()
+});
+
+pub static USER_KEY: LazyLock<Uuid> = LazyLock::new(|| {
+    let path = "sdmc:/3ds/Cloudpoint/user.key";
+
+    if fs::exists(path).unwrap_or_default() {
+        let raw = fs::read_to_string(&path).expect("userkey should be accessible");
+        let userkey = Uuid::try_parse(&raw).expect("userkey should be a uuid");
+
+        userkey
+    } else {
+        let userkey = Uuid::new_v4();
+        let mut buf = Uuid::encode_buffer();
+        fs::write(&path, userkey.as_simple().encode_lower(&mut buf))
+            .expect("userkey should be writable");
+
+        userkey
+    }
 });
