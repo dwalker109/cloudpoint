991f45af0a96e94b417dde50a0e1503e719005be
Dan Walker <git@dwalker.me>
2026-05-19 00:07:09 +0000

    Refactor https_curl function to check target before setting up environment for armv6k-nintendo-3ds

--- cloudpoint_lib/build.rs
+++ cloudpoint_lib/build.rs
@@ -6,11 +6,15 @@ fn main() {
 }
 
 fn https_curl() {
-    let devkitpro = std::env::var("DEVKITPRO").expect("DEVKITPRO not set");
-    let portlibs = format!("{devkitpro}/portlibs/3ds/lib");
-    println!("cargo:rustc-link-search=native={portlibs}");
-    println!("cargo:rustc-link-lib=static=mbedtls");
-    println!("cargo:rustc-link-lib=static=mbedx509");
-    println!("cargo:rustc-link-lib=static=mbedcrypto");
-    println!("cargo:rustc-link-lib=static=z");
+    let target = std::env::var("TARGET").unwrap();
+
+    if target == "armv6k-nintendo-3ds" {
+        let devkitpro = std::env::var("DEVKITPRO").expect("DEVKITPRO not set");
+        let portlibs = format!("{devkitpro}/portlibs/3ds/lib");
+        println!("cargo:rustc-link-search=native={portlibs}");
+        println!("cargo:rustc-link-lib=static=mbedtls");
+        println!("cargo:rustc-link-lib=static=mbedx509");
+        println!("cargo:rustc-link-lib=static=mbedcrypto");
+        println!("cargo:rustc-link-lib=static=z");
+    } 
 }
